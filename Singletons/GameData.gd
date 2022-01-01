bd7e44fc94294fcc6af1427deeed2940dd172590
Cavan <cavan.biggs@circumference.ca>
2022-01-01 00:40:58 -0500

    Added impact animations

--- Singletons/GameData.gd
+++ Singletons/GameData.gd
@@ -3,12 +3,14 @@ extends Node
 var tower_data = {
 	"GunT1":{
 		"damage":20,
-		"rof":1,
-		"range":350},
+		"rof":0.3,
+		"range":350,
+		"category": "Projectile"},
 	"MissileT1":{
 		"damage":20,
 		"rof":3,
-		"range":550}
+		"range":550,
+		"category":"Missile"}
 	
 }
 
